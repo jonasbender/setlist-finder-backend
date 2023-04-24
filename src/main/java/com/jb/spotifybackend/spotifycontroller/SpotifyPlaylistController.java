c6638fec4193f6413caa516f5007f4b5e1dcf397
jonasbender <contact@jonasbender.net>
2023-04-24 22:18:15 +0200

    created SpotifyPlaylistController class

---
+++ src/main/java/com/jb/spotifybackend/spotifycontroller/SpotifyPlaylistController.java
@@ -0,0 +1,13 @@
+package com.jb.spotifybackend.spotifycontroller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SpotifyPlaylistController {
+
+
+
+}
