fd5a84af1cd688472b7b38781d1bca516a4bdfae
jonasbender <contact@jonasbender.net>
2023-05-01 21:19:05 +0200

    removed temporary code

--- src/main/java/com/jb/spotifybackend/spotifycontroller/SpotifyPlaylistController.java
+++ src/main/java/com/jb/spotifybackend/spotifycontroller/SpotifyPlaylistController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class SpotifyPlaylistController {
 
+    public static void createPlaylist() {
+
+
+
+    }
 
 
 }
