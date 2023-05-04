996d2be9956d93c9c119bac515896aedc79e7438
jonasbender <contact@jonasbender.net>
2023-05-04 21:26:21 +0200

    created PlaylistRequestModel class

---
+++ src/main/java/com/jb/spotifybackend/model/PlaylistRequestModel.java
@@ -0,0 +1,60 @@
+package com.jb.spotifybackend.model;
+
+import java.util.List;
+
+public class PlaylistRequestModel {
+    private List<String> songIds;
+    private String playlistImageUrl;
+    private String currentUrl;
+    private String userId;
+    private String playlistName;
+    private boolean isPrivate;
+
+    public List<String> getSongIds() {
+        return songIds;
+    }
+
+    public void setSongIds(List<String> songIds) {
+        this.songIds = songIds;
+    }
+
+    public String getPlaylistImageUrl() {
+        return playlistImageUrl;
+    }
+
+    public void setPlaylistImageUrl(String playlistImageUrl) {
+        this.playlistImageUrl = playlistImageUrl;
+    }
+
+    public String getCurrentUrl() {
+        return currentUrl;
+    }
+
+    public void setCurrentUrl(String currentUrl) {
+        this.currentUrl = currentUrl;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+    public void setPrivate(boolean aPrivate) {
+        isPrivate = aPrivate;
+    }
+}
