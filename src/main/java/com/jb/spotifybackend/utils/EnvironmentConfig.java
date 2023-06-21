540e35600e4c391eb0e1a64b131ed120a3350f12
jonasbender <contact@jonasbender.net>
2023-06-21 20:08:56 +0200

    configured the environment

---
+++ src/main/java/com/jb/spotifybackend/utils/EnvironmentConfig.java
@@ -0,0 +1,16 @@
+package com.jb.spotifybackend.utils;
+
+public class EnvironmentConfig {
+
+    public static final String REDIRECT_URI;
+
+    static {
+        // Set the value based on the active profile
+        if ("prod".equals(System.getProperty("spring.profiles.active"))) {
+            REDIRECT_URI = "https://setlistfinderbackend-9a6bfa1f1e4a.herokuapp.com/api/callback/";
+        } else {
+            REDIRECT_URI = "http://localhost:8080/api/callback";
+        }
+    }
+
+}
