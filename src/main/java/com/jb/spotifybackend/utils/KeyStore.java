52fb503bb7a1364d8f4fc98df8170b4cde060824
jonasbender <contact@jonasbender.net>
2023-09-01 09:22:33 +0200

    removed sensitive information

--- src/main/java/com/jb/spotifybackend/utils/KeyStore.java
+++ src/main/java/com/jb/spotifybackend/utils/KeyStore.java
@@ -11,10 +11,8 @@ public class KeyStore {
     //public static final URI redirectUri = SpotifyHttpManager.makeUri("https://setlistfinderbackend-9a6bfa1f1e4a.herokuapp.com/api/callback/");
 
     public static final URI redirectUri = URI.create(EnvironmentConfig.REDIRECT_URI);
-    public static final String CLIENT_ID = "013cc9a435c7467492532478e1144d3c";
-    public static final String CLIENT_SECRET = "746bc9dc80924d48804b351fcba66356";
 
-    // setlist.fm API Token
-
-    public static final String API_KEY = "c35We3GBKCaoF21NOBOeDQobebn9ttQ8wtzq";
+    public static final String CLIENT_ID = System.getenv("CLIENT_ID");
+    public static final String CLIENT_SECRET = System.getenv("CLIENT_SECRET");
+    public static final String API_KEY = System.getenv("API_KEY");
 }
