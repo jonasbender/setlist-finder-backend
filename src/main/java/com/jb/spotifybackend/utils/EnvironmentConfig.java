8e0cd405751a1f3df0a31f7f26cb4cf4b6934ed9
jonasbender <contact@jonasbender.net>
2023-09-01 09:28:23 +0200

    removed sensitive information

--- src/main/java/com/jb/spotifybackend/utils/EnvironmentConfig.java
+++ src/main/java/com/jb/spotifybackend/utils/EnvironmentConfig.java
@@ -9,7 +9,9 @@ public class EnvironmentConfig {
         if ("prod".equals(System.getProperty("spring.profiles.active"))) {
             REDIRECT_URI = "https://setlistfinderbackend-9a6bfa1f1e4a.herokuapp.com/api/callback/";
         } else {
-            REDIRECT_URI = "http://localhost:8080/api/callback";
+            REDIRECT_URI = "https://setlistfinderbackend-9a6bfa1f1e4a.herokuapp.com/api/callback/";
+
+            //REDIRECT_URI = "http://localhost:8080/api/callback";
         }
     }
 
